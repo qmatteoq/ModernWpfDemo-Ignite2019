49fa055fdb7c83ef1adfa178b62a6691f392b23a
Matteo Pagani <info@qmatteoq.com>
2019-10-23 14:48:18 +0200

    Add project files.

---
+++ ModernWpfDemo/ComplexModel.xaml.cs
@@ -0,0 +1,35 @@
+﻿using EngineComponent;
+using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ModernWpfDemo
+{
+    /// <summary>
+    /// Interaction logic for ComplexModel.xaml
+    /// </summary>
+    public partial class ComplexModel : Window
+    {
+        public ComplexModel()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+            if (sender is WindowsXamlHost windowsXamlHost && windowsXamlHost.Child is EngineModel engineModel)
+            {
+                engineModel.ModelUrl = new Uri("ms-appx:///Model/Telescope.gltf");
+            }
+        }
+    }
+}
---
+++ ModernWpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+
+namespace ModernWpfDemo
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Geolocator geolocator = new Geolocator() { DesiredAccuracyInMeters = 5 };
+            Geoposition pos = await geolocator.GetGeopositionAsync();
+            string longitude = pos.Coordinate.Point.Position.Longitude.ToString();
+            string latitude = pos.Coordinate.Point.Position.Latitude.ToString();
+            LongBlock.Text = longitude.Substring(0, longitude.IndexOf('.') + 3);
+            LatBlock.Text = latitude.Substring(0, latitude.IndexOf('.') + 3);
+
+            await TheMap.TrySetViewAsync(pos.Coordinate.Point, 15.0);
+        }
+
+        private void OnOpenModel(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ComplexModel complexModel = new ComplexModel();
+            complexModel.Show();
+        }
+    }
+}
